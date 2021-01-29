f6e29a01094e6790506ff1d5bcfc126d377f7ffd
mahfuzon <mahfuzon0@gmail.com>
2021-01-29 22:14:34 +0700

    menambahkan link untuk perpindahan menu di template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('Customer') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('Driver') }}">{{ __('Driver') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
