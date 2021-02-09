024cf62f1ad0addf36bea3659c41ad65be085c6e
mahfuzon <mahfuzon0@gmail.com>
2021-02-09 14:57:57 +0700

    membuat tampilan filter tanggal dan update fitur update tanggal pada component

--- app/Http/Livewire/ShowCustomer.php
+++ app/Http/Livewire/ShowCustomer.php
@@ -10,9 +10,13 @@ class ShowCustomer extends Component
 {
     protected $paginationTheme = 'bootstrap';
     use WithPagination;
-    public $customer_id;
+    public $customer_id, $to;
+    public $from;
+
     public function render()
     {
-        return view('livewire.show-customer', ['customer' => Customer::findOrFail($this->customer_id)->transaction()->latest()->paginate(3)]);
+        return view('livewire.show-customer', ['customer' => $this->from === null && $this->to === null ? Customer::findOrFail($this->customer_id)->transaction()->latest()->paginate(3):
+        Customer::findOrFail($this->customer_id)->transaction()->latest()->whereBetween('date', [$this->from, $this->to])->paginate(3)
+        ]);
     }
 }
--- resources/views/livewire/show-customer.blade.php
+++ resources/views/livewire/show-customer.blade.php
@@ -1,30 +1,51 @@
 <div>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Tanggal Transaksi</th>
-                <th>Total Berat</th>
-                <th>Total Harga</th>
-                <th>Bayar</th>
-                <th>Hutang</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $i = 1
-            @endphp
-            @foreach ($customer as $item)
-                <tr>
-                    <td>{{$i++}}</td>
-                    <td>{{$item->date}}</td>
-                    <td>{{$item->total_berat}}</td>
-                    <td>{{$item->total_harga}}</td>
-                    <td>{{$item->bayar}}</td>
-                    <td>{{$item->hutang}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $customer->links() }}
+  <form action="{{route('export_pdf')}}" method="post">
+    @csrf
+    <div class="row g-5 align-items-center">
+      <div class="col-auto">
+        <label for="from" class="col-form-label">From:</label>
+      </div>
+      <div class="col-auto">
+        <input type="date" id="from" class="form-control" wire:model='from' name="from">
+      </div>
+      <div class="col-auto">
+        <label for="to" class="col-form-label">To:</label>
+      </div>
+      <div class="col-auto">
+        <input type="date" id="to" class="form-control" wire:model='to' name="to">
+      </div>
+      <div class="col-auto">
+        <input type="submit" id="submit" class="form-control btn btn-success" name="submit" value="PDF">
+      </div>
+    </div>
+  </form>
+  
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Tanggal Transaksi</th>
+        <th>Total Berat</th>
+        <th>Total Harga</th>
+        <th>Bayar</th>
+        <th>Hutang</th>
+      </tr>
+    </thead>
+    <tbody>
+      @php
+      $i = 1
+      @endphp
+      @foreach ($customer as $item)
+      <tr>
+        <td>{{$i++}}</td>
+        <td>{{$item->date}}</td>
+        <td>{{$item->total_berat}}</td>
+        <td>{{$item->total_harga}}</td>
+        <td>{{$item->bayar}}</td>
+        <td>{{$item->hutang}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  {{ $customer->links() }}
 </div>
