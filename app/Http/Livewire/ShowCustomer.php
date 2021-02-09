6377947bc57467f089ff78063fc6706b9fc7e547
mahfuzon <mahfuzon0@gmail.com>
2021-02-09 13:08:19 +0700

    berhasil menampilkan data dalam tabel

--- app/Http/Livewire/ShowCustomer.php
+++ app/Http/Livewire/ShowCustomer.php
@@ -3,12 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Customer;
 
 class ShowCustomer extends Component
 {
     public $customer_id;
     public function render()
     {
-        return view('livewire.show-customer');
+        return view('livewire.show-customer', ['customer' => Customer::findOrFail($this->customer_id)->transaction()->latest()->get()]);
     }
 }
--- resources/views/livewire/show-customer.blade.php
+++ resources/views/livewire/show-customer.blade.php
@@ -1,3 +1,29 @@
 <div>
-    {{$customer_id}}
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Tanggal Transaksi</th>
+                <th>Total Berat</th>
+                <th>Total Harga</th>
+                <th>Bayar</th>
+                <th>Hutang</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $i = 1
+            @endphp
+            @foreach ($customer as $item)
+                <tr>
+                    <td>{{$i++}}</td>
+                    <td>{{$item->date}}</td>
+                    <td>{{$item->total_berat}}</td>
+                    <td>{{$item->total_harga}}</td>
+                    <td>{{$item->bayar}}</td>
+                    <td>{{$item->hutang}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
