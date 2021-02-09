4347ffa2f753ce7d1569cfd1ab66a5c690291071
mahfuzon <mahfuzon0@gmail.com>
2021-02-09 14:39:15 +0700

    berhasil membuat paginasi d customer detail

--- app/Http/Livewire/ShowCustomer.php
+++ app/Http/Livewire/ShowCustomer.php
@@ -4,12 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Customer;
+use Livewire\WithPagination;
 
 class ShowCustomer extends Component
 {
+    protected $paginationTheme = 'bootstrap';
+    use WithPagination;
     public $customer_id;
     public function render()
     {
-        return view('livewire.show-customer', ['customer' => Customer::findOrFail($this->customer_id)->transaction()->latest()->get()]);
+        return view('livewire.show-customer', ['customer' => Customer::findOrFail($this->customer_id)->transaction()->latest()->paginate(3)]);
     }
 }
