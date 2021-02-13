0b901ad9698408ba67bea4927c8c61e48fe3c7fb
mahfuzon <mahfuzon0@gmail.com>
2021-02-13 14:44:49 +0700

    membuat component DetailModal

---
+++ app/Http/Livewire/DetailModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DetailModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.detail-modal');
+    }
+}
